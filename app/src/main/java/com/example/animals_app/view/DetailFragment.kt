f18682fa92775e211ff0cb0078c1596618d7a2a8
Darnell10 <DarnellOtto@ac.c4q.nyc>
2020-03-30 02:20:53 -0400

    binded view to detailfragment using Databinding libaries

--- app/src/main/java/com/example/animals_app/view/DetailFragment.kt
+++ app/src/main/java/com/example/animals_app/view/DetailFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.example.animals_app.R
+import com.example.animals_app.databinding.FragmentDetailBinding
 import com.example.animals_app.model.Animal
 import com.example.animals_app.util.getProgressDrawable
 import com.example.animals_app.util.loadImage
@@ -17,12 +19,18 @@ class DetailFragment : Fragment() {
 
     var animal: Animal? = null
 
+    private lateinit var dataBinding: FragmentDetailBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        dataBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_detail, container,
+            false
+        )
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -31,14 +39,16 @@ class DetailFragment : Fragment() {
         arguments?.let {
             animal = DetailFragmentArgs.fromBundle(it).animal
         }
-        context?.let {
-            animalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
-        }
+//        context?.let {
+//            dataBinding.animalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
+//        }
 
-        animalName.text = animal?.name
-        animalLocation.text = animal?.location
-        animalLifeSpan.text = animal?.lifeSpan
-        animalDiet.text = animal?.diet
+        dataBinding.animal = animal
+
+//        animalName.text = animal?.name
+//        animalLocation.text = animal?.location
+//        animalLifeSpan.text = animal?.lifeSpan
+//        animalDiet.text = animal?.diet
 
     }
 
